88407f74ea2020a54e388a1ae3d1810ceb8a318f
yuvarajSasi <yuvisasi315@gmail.com>
2025-06-21 16:10:24 +0530

    skills

--- learn/src/App.jsx
+++ learn/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Skill from './Skill.jsx'
+import Skill from './Skills.jsx'
 import './App.css'
 import {Route, Routes} from 'react-router-dom'
 import Home from './Home.jsx'
